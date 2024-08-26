e0b6975b94958d4cc3dc20f0fc5b4cd5734018b6
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-08-26 18:53:38 +0200

    Init

---
+++ fetch-chadsoft.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+
+async function fetchChadsoft() {
+    let url = "https://tt.chadsoft.co.uk/index.json";
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let recentCtTopFive = data.recentRecords.filter((record) => record.defaultTrack === false && record["200cc"] === false);
+
+    
+    fs.writeFile("recentRuns.json", JSON.stringify(recentCtTopFive, null, 4), (err) => {
+      if (err) {
+        console.error("Error writing JSON to file:", err);
+      } else {
+        console.log("JSON data written to file successfully.");
+      }
+    });
+    
+}
+
+fetchChadsoft();
