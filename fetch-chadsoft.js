8b676d182815cbed781ee829b4351c34133a3d55
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-08-27 09:57:46 +0200

    Minor changes

--- fetch-chadsoft.js
+++ fetch-chadsoft.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 async function fetchChadsoft() {
     let url = "https://tt.chadsoft.co.uk/index.json";
     let response = await fetch(url);
@@ -8,8 +7,8 @@ async function fetchChadsoft() {
 
     let recentCtTopFive = {
       recentRuns: data.recentRecords.filter((record) => record.defaultTrack === false && record["200cc"] === false),
-      lastUpdated: new Date().toLocaleString()
-    }
+      lastUpdated: new Date()
+    };
 
     
     fs.writeFile("recentRuns.json", JSON.stringify(recentCtTopFive, null, 4), (err) => {
